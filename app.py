798634125bbaec6f2a70bb043e24df6eb1fa8e43
Yasmeen Roumie <y4smeen@gmail.com>
2016-01-31 00:04:42 -0500

    stuff

--- app.py
+++ app.py
@@ -1,32 +1,18 @@
 from flask import Flask, render_template, request, session
-import concepts, search2, secret
-from flask.ext.twitter_oembedder import TwitterOEmbedder
-from flask.ext.cache import Cache
-
-twitter_oembedder = TwitterOEmbedder()
+import concepts, twitter, secret, twitter2
 
 app = Flask(__name__)
-cache = Cache(app)
-
-twitter_oembedder.init(app,cache)
-
-# CONFIG
-
-app.config['TWITTER_CONSUMER_KEY'] = "b7YJCQN3XYOvxIevp1o5mc684"
-app.config['TWITTER_CONSUMER_SECRET'] = "cEDvjmnGl1KbnbwEW8v5hjPXWnIRnwm2FNuS9L0SkFfhiYQYQD"
-app.config['TWITTER_ACCESS_TOKEN'] = "3090270271-VzR6iBYUGdM69zan3WJ8OhApGLtZmjijxUNeDeL"
-app.config['TWITTER_TOKEN_SECRET'] = "PCIlN6jPIxuhwMlS1eOW7i7fQo14WFqVhsGqa7AidFHBv"
 
 @app.route('/', methods=["GET", "POST"])
 def index():
     if (request.method == "GET"):
         return render_template("index.html")
-    elif (request.form['search'] != "" and request.method == "POST"):
+    else:
         query = request.form['search']
-        tweets = search2.
+        tweets = twitter.
         results = concepts.getConcepts(query)
-        return render_template("index.html", keyword_list=results, query=query, tweets=tweets)
-
+        return render_template("index.html", keyword_list=results, query=query, tweets=tweets, htmls=html_list[:5])
+        html_list = twitter2.getEmbed()
 
 if __name__=="__main__":
     app.debug = True
