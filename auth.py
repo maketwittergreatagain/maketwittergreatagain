a2a9b861fa1290c32fba892095d6f53ee57df571
Yasmeen Roumie <y4smeen@gmail.com>
2016-01-30 21:01:12 -0500

    move

--- auth.py
+++ auth.py
@@ -2,7 +2,3 @@ consumer_key = 'RIPARgjKRSPiQkoQ4Py16zwet'
 consumer_secret = 'jt531pZpzTAEHRGUxhuJyOsffhKE2YvcqzGxwCB8PAhhfVYGzL'
 access_token = '938717683-34jKorRGw6fgBkIpubba0Hf4FDF7i9FLwueGgClP'
 access_token_secret = '6APD2gFJoncd2bGkudh0uGiPlSppvfye93QfrV2jD1Edo'
-
-
-
-
