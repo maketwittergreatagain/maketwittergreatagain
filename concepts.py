faeb54eaa16ae68b19b40a64be86e7e2044fea14
Yasmeen Roumie <y4smeen@gmail.com>
2016-01-30 19:16:35 -0500

    attempt twitter stuff

--- concepts.py
+++ concepts.py
@@ -14,11 +14,11 @@ def getConcepts(seed):
     job_id = concept_expansion.create_job(dataset='mtsamples', seeds=[seed]) #, label='medications')
     #print(json.dumps(job_id, indent=2))
 
-    time.sleep(5)  # sleep for 5 seconds
+    time.sleep(1)  # sleep for 5 seconds
     job_status = concept_expansion.get_status(job_id)
 
     while job_status['status'] == 'in progress' or job_status['status'] == 'awaiting work':
-        time.sleep(5)  # sleep for 5 seconds
+        time.sleep(1)  # sleep for 5 seconds
         job_status = concept_expansion.get_status(job_id)
         print(json.dumps(job_status, indent=2))
 
